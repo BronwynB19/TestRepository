1d01d9e1c52b6d59afc733326bad122774294549
Bronwyn19 <bbrett23171@gmail.com>
2023-11-13 14:38:36 -0500

    Update Test File.py
    Added more comments

--- Test File.py
+++ Test File.py
@@ -4,3 +4,13 @@
 I figured out how to add files
     Just make a new file and save it in the repository directory
 """
+
+"""
+Now I gotta learn about 
+    Push
+    Pull
+    Fetch
+    Remove
+    Branch
+"""
+
