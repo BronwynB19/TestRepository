f19eab25234721af842b717acb14b23c099207bb
Bronwyn19 <bbrett23171@gmail.com>
2023-11-13 15:22:11 -0500

    Create test_file.py

---
+++ test_file.py
@@ -0,0 +1,34 @@
+# Test Repository File
+
+"""
+I figured out how to add files
+    Just make a new file and save it in the repository directory
+"""
+
+"""
+Now I gotta learn about 
+    Commit
+        Updates local repository
+    Push
+        Updates remote repository(GitHub) (i think u only do this when it's 100% ready to go in)
+    Pull
+        Updates your local branch with changes from the remote repository and merges them into your current branch.
+    Fetch
+        Fetches changes from the remote repository but doesn't automatically merge them into your current branch
+    Remove
+    Branch commands
+        git branch: shows branches and current branch
+        git branch -c new_branch_name: creates a new branch with new_branch_name
+        git checkout branch_name: sets current working branch to branch_name
+
+"""
+
+"""
+When you move branches without committing(on desktop), changes get stashed and need to be restored and committed
+"""
+
+"""
+how to branch a branch
+my git status doesnt give me "up to date" like the video, says my branch is ahead by 3 commits
+"""
+
